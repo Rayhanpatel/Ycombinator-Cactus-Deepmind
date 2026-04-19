8556e6d783952c7c352033cdf55c68c4af6164dd
tash-2s <81064017+tash-2s@users.noreply.github.com>
2026-04-18 17:38:31 -0700

    ui scaffold

---
+++ ui/ui/uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiApp.swift
+//  ui
+//
+//  Created by t on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

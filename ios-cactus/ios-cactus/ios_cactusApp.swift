a99eecf5417053c2bbed8dba941c1827d2537790
tash-2s <81064017+tash-2s@users.noreply.github.com>
2026-04-18 20:17:35 -0700

    scaffold

---
+++ ios-cactus/ios-cactus/ios_cactusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cactusApp.swift
+//  ios-cactus
+//
+//  Created by t on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct ios_cactusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
